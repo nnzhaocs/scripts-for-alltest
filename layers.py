f9d1537bd6657f5487ab22791b6075cb54c7f494
Nannan Zhao <nannanzhao@nannans-mbp.almaden.ibm.com>
2017-06-23 11:13:21 -0700

    fixing logging19

--- layers.py
+++ layers.py
@@ -93,7 +93,7 @@ def load_layer(f_out):
         json.dump(layer, f_out)
         lock.release()
 
-        logging.debug('layer_id:[%s]: %s', layer_id, layer)
+        logging.debug('write layer_id:[%s]: %s to json file', layer_id, layer)
 
 
 # def load_layerBychainid(chainid):
