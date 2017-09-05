8f5af85a8e87478479e5cb280a0c6c4c2fb5ad90
nnzhaocs <znannan1@vt.edu>
2017-09-05 18:42:54 -0400

    seperate metric json files

--- scripts/plotter/generate_job_list.py
+++ scripts/plotter/generate_job_list.py
@@ -1,152 +1,358 @@
-
-# from imports import *
-# from utility import *
 from draw_pic import *
 
-q_analyzed_layer_jsons = []
-q_dir_layer_jsons = []
+image_mappers = []
+layer_mappers = []
 
+image_metrics_data = []
+"""image_metrics_data content
 
-def run_getmetricsdata():
-    logging.info('=============> run_getmetricsdata <===========')
-    queue_layer_jsons()
-    print "create pool"
-    P = multiprocessing.Pool(60)
-    print "before map"
-    q_analyzed_layer_jsons = P.map(load_layer_json, q_dir_layer_jsons)
-    #P.close()
-    #P.join()
-    print "after map"
-    for i in q_analyzed_layer_jsons:
-	#while not q_analyzed_layer_json.empty():
-        print i
+    version: schema1:
+            schema2:
+            schemalist:
+    size: compressed
+            sum of 
+            archival
+    compression ratio:
+    repeat layer cnt:
+    file cnt:
+    layer depth:
+"""
 
-    with open("saved_analyzed_layer_jsonfile.out", 'w+') as f_analyzed_layer:
-        json.dump(q_analyzed_layer_jsons, f_analyzed_layer)
-    #distribution_plot()
+layer_metrics_datas = []
+"""layer_metrics_data content
+    size: compressed
+            sum of 
+            archival
+    compression ratio:
+    repeat file cnt:
+    file cnt:
+    layer depth:
+    file size:
+    file type:
+
+"""
+
+file_metrics_datas = []
+"""file_metrics_data content
+    file size:
+    file type:
+    file extension:
+    sha256:
+    ctime
+    mtime
+    atime
+    uid
+    gid
+"""
 
 
 def run_generatejoblist():
+    logging.info('=============> run_generatejoblist <===========')
+
+    load_image_mappers()
+    load_layers_mappers()
+
+    calculate_repeat_layer_in_images()
+
+    print "create pool"
+    P1 = multiprocessing.Pool(60)
+    print "before map"
+    image_metrics_datas = P1.map(load_image_metrics_data, image_mappers)
+    print "after map"
+
+    with open("image_metrics_datas.json", 'w+') as f_image_metrics_data:
+        json.dump(image_metrics_datas, f_image_metrics_data)
+
+    print "create pool"
+    P2 = multiprocessing.Pool(60)
+    print "before map"
+    layer_metrics_datas = P2.map(load_layer_metrics_data, layer_mappers)
+    print "after map"
+
+    with open("layer_metrics_datas.json", 'w+') as f_layer_metrics_datas:
+        json.dump(layer_metrics_datas, f_layer_metrics_datas)
+
+    calaculate_file_metrics()
 
 
-def load_layer_json(layer_json_filename):
-    layer_db_json_dir = dest_dir[0]['layer_db_json_dir']
-    layer_base_info = {}
-    if layer_json_filename is None:
-	# print 'The dir layer json queue is empty!'
-        laogging.debug('The dir layer json queue is empty!')
-        return None
+def load_layer_metrics_data(layer_mapper):
+    layer_dir_files = []
+    layer_metrics_data = {}
 
-    if not os.path.isfile(os.path.join(layer_db_json_dir, layer_json_filename)):
-        logging.debug("layer json file %s is not valid!", layer_json_filename)
-        return None
+    uncompressed_sum_of_files = 0
+    compressed_size_with_method_gzip = 0
+    archival_size = 0
+    file_cnt = 0
 
-    logging.debug('process layer_json file: %s', layer_json_filename)  # str(layer_id).replace("/", "")
+    dir_max_depth = 0
+    dir_min_depth = 0
+    dir_median_depth = 0
+    dir_avg_depth = 0
 
-    # try:
-    #    lj_f = open(os.path.join(layer_db_json_dir, layer_json_filename))
-    ## except:
-    #    logging.debug("cannot open layer json file, %s" % layer_json_filename)
-    #    return None
-    #    lj_f.close()
+    dir_cnt = 0
 
-    with open(os.path.join(layer_db_json_dir, layer_json_filename)) as lj_f:
-        try:
-            json_data = json.load(lj_f)
-        except:
-	    # print "cannot load json file: layer json file %s is not valid!"
-            logging.debug("cannot load json file: layer json file %s is not valid!", layer_json_filename)
-            lj_f.close()
-            return None
+    for key, val in layer_mapper.items():  # only one entry
+        logging.debug("key: %s, val: %s!", key, val)
+        layer_json_absfilename = val
 
-        uncompressed_sum_of_files = json_data['size']['uncompressed_sum_of_files']
-        compressed_size_with_method_gzip = json_data['size']['compressed_size_with_method_gzip']
-        archival_size = json_data['size']['archival_size']
+        if not os.path.isfile(layer_json_absfilename):
+            logging.debug("layer json file %s is not valid!", layer_json_absfilename)
+            continue
 
-        dir_max_depth = json_data['layer_depth']['dir_max_depth']
+        logging.debug('process layer_json file: %s', layer_json_absfilename)  # str(layer_id).replace("/", "")
 
-        file_cnt = json_data['file_cnt']
+        with open(layer_json_absfilename) as lj_f:
+            try:
+                json_data = json.load(lj_f)
+            except:
+                logging.debug("cannot load json file: layer json file %s is not valid!", layer_json_absfilename)
+                lj_f.close()
+                continue
 
-        del json_data
+            uncompressed_sum_of_files = json_data['size']['uncompressed_sum_of_files']
+            compressed_size_with_method_gzip = json_data['size']['compressed_size_with_method_gzip']
+            archival_size = json_data['size']['archival_size']
 
-    #layer_base_info = [uncompressed_sum_of_files, compressed_size_with_method_gzip, archival_size, dir_max_depth, file_cnt]
+            dir_max_depth = json_data['layer_depth']['dir_max_depth']
+            dir_min_depth = json_data['layer_depth']['dir_min_depth']
+            dir_median_depth = json_data['layer_depth']['dir_median_depth']
+            dir_avg_depth = json_data['layer_depth']['dir_avg_depth']
 
-    layer_base_info['uncompressed_sum_of_files'] = uncompressed_sum_of_files
-    layer_base_info['compressed_size_with_method_gzip'] = compressed_size_with_method_gzip
-    layer_base_info['archival_size'] = archival_size
+            file_cnt = json_data['file_cnt']
+            dir_cnt = len(json_data['sub_dirs'])
 
-    layer_base_info['dir_max_depth'] = dir_max_depth
-    layer_base_info['file_cnt'] = file_cnt
-    layer_base_info['sum_to_gzip_ratio'] = uncompressed_sum_of_files*1.0/compressed_size_with_method_gzip
-    layer_base_info['archival_to_gzip_ratio'] = archival_size*1.0/compressed_size_with_method_gzip
-    logging.debug("Put tuple: %s to queues!", layer_base_info)
-    return layer_base_info
-    #q_analyzed_layer_jsons.put(layer_base_info)
+            for subdir in json_data['sub_dirs']:
+                for sub_file in subdir['files']:
+                    layer_dir_files.append(sub_file)
 
+            del json_data
 
-def queue_layer_jsons():
+    layer_metrics_data['uncompressed_sum_of_files'] = uncompressed_sum_of_files
+    layer_metrics_data['compressed_size_with_method_gzip'] = compressed_size_with_method_gzip
+    layer_metrics_data['archival_size'] = archival_size
 
-    """write image mapper"""
-    # if image_mappers:
-    # with open(os.path.join(dest_dir[0]['job_list_dir'],'image_mapper.json'), 'w') as f:
-    #     json.dump(image_mappers, f)
-    # if bad_image_mapper:
-    with open(os.path.join(dest_dir[0]['job_list_dir'],'bad_image_mapper.json'), 'w') as f:
-        json.dump(bad_image_mapper, f)
+    layer_metrics_data['sum_to_gzip_ratio'] = uncompressed_sum_of_files * 1.0 / compressed_size_with_method_gzip
+    layer_metrics_data['archival_to_gzip_ratio'] = archival_size * 1.0 / compressed_size_with_method_gzip
 
-    layer_dir = dest_dir[0]['layer_db_json_dir']
-    i = 0
-    for path, _, layer_json_filenames in os.walk(layer_dir):
-        for layer_json_filename in layer_json_filenames:
-            i = i + 1
-            #if i > 50:
-             #   break
-            # logging.debug('layer_id: %s', layer_json_filename)  # str(layer_id).replace("/", "")
-            q_dir_layer_jsons.append(layer_json_filename)
-            logging.debug('queue dir layer json file: %s', layer_json_filename)  # str(layer_id).replace("/", "")
+    layer_metrics_data['dir_max_depth'] = dir_max_depth
+    layer_metrics_data['dir_min_depth'] = dir_min_depth
+    layer_metrics_data['dir_median_depth'] = dir_median_depth
+    layer_metrics_data['dir_avg_depth'] = dir_avg_depth
+
+    layer_metrics_data['file_cnt'] = file_cnt
+    layer_metrics_data['dir_cnt'] = dir_cnt
+
+    layer_metrics_data['files'] = layer_dir_files
+
+    logging.debug("layer_metrics_data: %s", layer_metrics_data)
+
+    return layer_metrics_data
 
 
-#def distribution_plot():
-#    layer_base_info = {}
-#    size = defaultdict(list)
-#    dir_depth = defaultdict(list)
-#    file_cnt = []
-#
-#    print "===============> start ploting <=============="
-#
-#    # for q_analyzed_layers in q_analyzed_layer_jsons:
-#    while not q_analyzed_layer_jsons.empty():
-#        json_data = q_analyzed_layer_jsons.get()
-#        print json_data
-#        #size['uncompressed_sum_of_files'].append(json_data[0])
-#        #size['compressed_size_with_method_gzip'].append(json_data[1])
-#        #size['archival_size'].append(json_data[2])
-#
-#        #dir_depth['dir_max_depth'].append(json_data[3])
-#
-#        #file_cnt.append(json_data[4])
-#
-#        size['uncompressed_sum_of_files'].append(json_data['uncompressed_sum_of_files'] / 1024 / 1024)
-#        size['compressed_size_with_method_gzip'].append(json_data['compressed_size_with_method_gzip'] / 1024 / 1024)
-#        size['archival_size'].append(json_data['archival_size'] / 1024 / 1024)
-#
-#        dir_depth['dir_max_depth'].append(json_data['dir_max_depth'])
-#
-#        file_cnt.append(json_data['file_cnt'])
-#
-#    layer_base_info['size'] = size
-#    layer_base_info['dir_depth'] = dir_depth
-#    layer_base_info['file_cnt'] = file_cnt
-#
-#    with open("saved_analyzed_layer_jsonfile.out", 'a+') as f_analyzed_layer:
-#        json.dump(layer_base_info, f_analyzed_layer)
-#
-#    fig = fig_size('small')  # 'large'
-#
-#    data1 = layer_base_info['size']['uncompressed_sum_of_files']
-#    xlabel = 'Uncompressed layer size (MB): sum of files'
-#    xlim = mean(data1)  # max(data1)
-#    ticks = 25
-#    print xlim
-#    plot_cdf(fig, data1, xlabel, xlim, ticks)
+def load_image_metrics_data(image_mapper):
+    # image_mapper = {
+    #     'version': version,
+    #     'manifest': manifest_name_dir_map{},
+    #     'config': config_name_dir_map{},
+    #     'layers': layers_map{:{:}}
+    # }
+
+    uncompressed_sum_of_files = 0
+    compressed_size_with_method_gzip = 0
+    archival_size = 0
+    file_cnt = 0
+
+    for key, val in image_mapper['layers'].items():
+        for key1, val1 in val.items():  # only one entry
+            layer_json_absfilename = val1  # json_absfilename
+            if layer_json_absfilename is None:
+                laogging.debug('The layer_json_absfilename is empty!')
+                continue
+
+            if not os.path.isfile(layer_json_absfilename):
+                logging.debug("layer json file %s is not valid!", layer_json_absfilename)
+                continue
+
+            logging.debug('process layer_json file: %s', layer_json_absfilename)  # str(layer_id).replace("/", "")
+
+            with open(layer_json_absfilename) as lj_f:
+                try:
+                    json_data = json.load(lj_f)
+                except:
+                    logging.debug("cannot load json file: layer json file %s is not valid!", layer_json_absfilename)
+                    lj_f.close()
+                    continue
+
+                uncompressed_sum_of_files = uncompressed_sum_of_files + json_data['size']['uncompressed_sum_of_files']
+                compressed_size_with_method_gzip = compressed_size_with_method_gzip + json_data['size'][
+                    'compressed_size_with_method_gzip']
+                archival_size = archival_size + json_data['size']['archival_size']
+
+                file_cnt = file_cnt + json_data['file_cnt']
+
+                del json_data
+
+    image_metrics_data['uncompressed_sum_of_files'] = uncompressed_sum_of_files
+    image_metrics_data['compressed_size_with_method_gzip'] = compressed_size_with_method_gzip
+    image_metrics_data['archival_size'] = archival_size
+
+    image_metrics_data['sum_to_gzip_ratio'] = uncompressed_sum_of_files * 1.0 / compressed_size_with_method_gzip
+    image_metrics_data['archival_to_gzip_ratio'] = archival_size * 1.0 / compressed_size_with_method_gzip
+
+    image_metrics_data['file_cnt'] = file_cnt
+
+    image_metrics_data['version'] = image_mapper['version']
+
+    logging.debug("image_metrics_data: %s", image_metrics_data)
+    return image_metrics_data
+
+
+def calculate_repeates(l):
+    logging.info("first file_sha256s list: %s", l[0])
+    l_dict = {i: l.count(i) for i in l}
+
+    for key, val in l.items():
+        logging.debug(key, val)
+
+    return l_dict
+
+
+def calaculate_file_metrics():
+    """get repeat files"""
+    for layer_metrics_data in layer_metrics_datas:
+        layer_dir_files = layer_metrics_data['files']
+        file_metrics_datas.append(layer_dir_files)
+
+    with open(os.path.join(dest_dir[0]['job_list_dir'], 'file_metrics_datas.json'), 'w') as f:
+        json.dump(file_metrics_datas, f)
+
+    file_sha256s = []
+    file_types = []
+    file_stat_types = []
+    file_stat_sizes = []
+
+    for dir_file in file_metrics_datas:
+        file_sha256s.append(dir_file['sha256'])
+
+    file_sha256s_dict = calculate_repeates(file_sha256s)
+
+    with open(os.path.join(dest_dir[0]['job_list_dir'], 'repeate_file_sha256s.json'), 'w') as f:
+        json.dump(file_sha256s_dict, f)
+
+    """file types"""
+
+    for dir_file in file_metrics_datas:
+        file_types.append(dir_file['type'])
+
+    file_types_dict = calculate_repeates(file_types)
+
+    with open(os.path.join(dest_dir[0]['job_list_dir'], 'repeate_file_types_dict.json'), 'w') as f:
+        json.dump(file_types_dict, f)
+
+    """file stat_type"""
+
+    for dir_file in file_metrics_datas:
+        file_stat_types.append(dir_file['file_info']['stat_type'])
+
+    file_types_dict = calculate_repeates(file_stat_types)
+
+    with open(os.path.join(dest_dir[0]['job_list_dir'], 'repeate_file_types_dict.json'), 'w') as f:
+        json.dump(file_types_dict, f)
+
+    """file size"""
+
+    for dir_file in file_metrics_datas:
+        file_stat_sizes.append(dir_file['file_info']['stat_size'])
+
+    file_stat_sizes_dict = calculate_repeates(file_stat_sizes)
+
+    with open(os.path.join(dest_dir[0]['job_list_dir'], 'repeate_file_stat_sizes_dict.json'), 'w') as f:
+        json.dump(file_stat_sizes_dict, f)
+
+
+def calculate_repeat_layer_in_images():
+    layer_digests = []
+
+    for image_mapper in image_mappers:
+        image_layer_digests = []
+        for key, val in image_mapper['layers'].items():
+            image_layer_digests.append(key)
+        layer_digests.append(image_layer_digests)
+
+    logging.info("first layer_digests list: %s", layer_digests[0])
+
+    layer_digests_union = list(chain(*layer_digests))
+
+    logging.info("first layer_digests list: %s", layer_digests_union[0])
+
+    """note that we remove the duplicates from schema 1's layer digests in contruct_image_mapper.py"""
+
+    layer_digests_dict = {i: layer_digests_union.count(i) for i in layer_digests_union}
+    for key, val in layer_digests_dict.items():
+        logging.debug(key, val)
+
+    with open(os.path.join(dest_dir[0]['job_list_dir'], 'repeate_layer_digests_dict.json'), 'w') as f:
+        json.dump(layer_digests_dict, f)
+
+
+def load_image_mappers():
+    """load_image_mappers"""
+    # image_mapper = {
+    #     'version': version,
+    #     'manifest': manifest_name_dir_map{},
+    #     'config': config_name_dir_map{},
+    #     'layers': layers_map{:{:}}
+    # }
+
+    with open(os.path.join(dest_dir[0]['job_list_dir'], 'bad_image_mapper.json'), 'r') as f:
+        _image_mappers = json.load(f)
+
+    for _image_mapper in _image_mappers:
+
+        manifest_name_dir_map = {}
+        config_name_dir_map = {}
+        layers_map = {}
+
+        for key, val in _image_mapper['manifest'].items():
+            manifest_name_dir_map[key] = val
+
+        if _image_mapper['config']:
+            for key, val in _image_mapper['config'].items():
+                config_name_dir_map[key] = val
+
+        if _image_mapper['layers']:
+            layer_name_dir_map = {}
+            for key, val in _image_mapper['layers'].items():
+                for key1, val1 in val.items():
+                    layer_name_dir_map[key1] = val1
+                layers_map[key] = layer_name_dir_map
+
+        image_mapper = {
+            'version': _image_mapper['version'],
+            'manifest': manifest_name_dir_map,
+            'config': config_name_dir_map,
+            'layers': layers_map,
+            'has_non_analyzed_layer_tarballs': _image_mapper['has_non_analyzed_layer_tarballs'],
+            'has_non_downloaded_config': _image_mapper['has_non_downloaded_config']
+        }
+
+        image_mappers.append(image_mapper)
+
+
+def load_layers_mappers():
+    layer_mapper = {}
+    for image_mapper in image_mappers:
+        for key, val in image_mapper['layers'].items():
+            for key1, val1 in val.items():  # only one entry
+                layer_json_absfilename = val1  # json_absfilename
+                layer_mapper[key] = layer_json_absfilename
+
+    with open(os.path.join(dest_dir[0]['job_list_dir'], 'layer_mappers.json'), 'w') as f:
+        json.dump(layer_mapper, f)
+
+    for key, val in layer_mapper:
+        tmp_mapper = {}
+        tmp_mapper[key] = val
+        layer_mappers.append(tmp_mapper)
+
 
