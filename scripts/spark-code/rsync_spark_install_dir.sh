a6242f6148c1eb1d48068cd14dfcf73ab1ceac15
nnzhaocs <znannan1@vt.edu>
2017-11-14 15:15:51 -0500

    rsync spark

--- scripts/spark-code/rsync_spark_install_dir.sh
+++ scripts/spark-code/rsync_spark_install_dir.sh
@@ -1,10 +1,10 @@
 
 #scp -r spark DataNode1:/usr/local
 
-sudo scp -r  /usr/local/spark nannan@hulk2:/usr/local/
-sudo scp -r  /usr/local/spark nannan@amaranth1:/usr/local/
-sudo scp -r  /usr/local/spark nannan@amaranth2:/usr/local/
-sudo scp -r  /usr/local/spark nannan@amaranth3:/usr/local/
-sudo scp -r  /usr/local/spark nannan@amaranth4:/usr/local/
-sudo scp -r  /usr/local/spark nannan@amaranth5:/usr/local/
-sudo scp -r  /usr/local/spark nannan@amaranth6:/usr/local/
+sudo scp -r  /usr/local/spark nannan@hulk2:~
+sudo scp -r  /usr/local/spark nannan@amaranth1:~
+sudo scp -r  /usr/local/spark nannan@amaranth2:~
+sudo scp -r  /usr/local/spark nannan@amaranth3:~
+sudo scp -r  /usr/local/spark nannan@amaranth4:~
+sudo scp -r  /usr/local/spark nannan@amaranth5:~
+sudo scp -r  /usr/local/spark nannan@amaranth6:~
