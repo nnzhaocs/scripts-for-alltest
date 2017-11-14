668b65a8d9e8673e2139220c0b3bca7e65617d81
nnzhaocs <znannan1@vt.edu>
2017-11-14 15:07:26 -0500

    rsync spark installation dir

---
+++ scripts/spark-code/rsync_spark_install_dir.sh
@@ -0,0 +1,10 @@
+
+#scp -r spark DataNode1:/usr/local
+
+scp -r  /usr/local/spark nannan@hulk2:/usr/local/
+scp -r  /usr/local/spark nannan@amaranth1:/usr/local/
+scp -r  /usr/local/spark nannan@amaranth2:/usr/local/
+scp -r  /usr/local/spark nannan@amaranth3:/usr/local/
+scp -r  /usr/local/spark nannan@amaranth4:/usr/local/
+scp -r  /usr/local/spark nannan@amaranth5:/usr/local/
+scp -r  /usr/local/spark nannan@amaranth6:/usr/local/
