72efeabfb02115a0e8ca244c0dfd289a934b2461
nnzhaocs <znannan1@vt.edu>
2017-11-14 15:01:31 -0500

    rsync datanodes

--- scripts/spark-code/rsync_spark_datanodes.sh
+++ scripts/spark-code/rsync_spark_datanodes.sh
@@ -1,8 +1,8 @@
 
-sudo rsync -avxP /home/nannan/spark/conf nannan@hulk2:/home/nannan/spark/conf
-sudo rsync -avxP /home/nannan/spark/conf nannan@amaranth1:/home/nannan/spark/conf
-sudo rsync -avxP /home/nannan/spark/conf nannan@amaranth2:/home/nannan/spark/conf
-sudo rsync -avxP /home/nannan/spark/conf nannan@amaranth3:/home/nannan/spark/conf
-sudo rsync -avxP /home/nannan/spark/conf nannan@amaranth4:/home/nannan/spark/conf
-sudo rsync -avxP /home/nannan/spark/conf nannan@amaranth5:/home/nannan/spark/conf
-sudo rsync -avxP /home/nannan/spark/conf nannan@amaranth6:/home/nannan/spark/conf
+sudo rsync -avxP /home/nannan/spark/conf/ nannan@hulk2:/home/nannan/spark/conf/
+sudo rsync -avxP /home/nannan/spark/conf/ nannan@amaranth1:/home/nannan/spark/conf/
+sudo rsync -avxP /home/nannan/spark/conf/ nannan@amaranth2:/home/nannan/spark/conf/
+sudo rsync -avxP /home/nannan/spark/conf/ nannan@amaranth3:/home/nannan/spark/conf/
+sudo rsync -avxP /home/nannan/spark/conf/ nannan@amaranth4:/home/nannan/spark/conf/
+sudo rsync -avxP /home/nannan/spark/conf/ nannan@amaranth5:/home/nannan/spark/conf/
+sudo rsync -avxP /home/nannan/spark/conf/ nannan@amaranth6:/home/nannan/spark/conf/
