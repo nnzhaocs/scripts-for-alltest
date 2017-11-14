69c6e7b0f279de92e4ebdca3150f6b7669d4c844
nnzhaocs <znannan1@vt.edu>
2017-11-14 14:46:45 -0500

    modify rsync hadoop

--- scripts/spark-code/rsync_datanodes.sh
+++ scripts/spark-code/rsync_datanodes.sh
@@ -1,8 +1,8 @@
 
-sudo rsync -avxP /home/nannan/hadoop/ nannan@hulk2:/home/nannan/hadoop/
-sudo rsync -avxP /home/nannan/hadoop/ nannan@amaranth1:/home/nannan/hadoop/
-sudo rsync -avxP /home/nannan/hadoop/ nannan@amaranth2:/home/nannan/hadoop/
-sudo rsync -avxP /home/nannan/hadoop/ nannan@amaranth3:/home/nannan/hadoop/
-sudo rsync -avxP /home/nannan/hadoop/ nannan@amaranth4:/home/nannan/hadoop/
-sudo rsync -avxP /home/nannan/hadoop/ nannan@amaranth5:/home/nannan/hadoop/
-sudo rsync -avxP /home/nannan/hadoop/ nannan@amaranth6:/home/nannan/hadoop/
+sudo rsync -avxP /home/nannan/hadoop/conf nannan@hulk2:/home/nannan/hadoop/conf
+sudo rsync -avxP /home/nannan/hadoop/conf nannan@amaranth1:/home/nannan/hadoop/conf
+sudo rsync -avxP /home/nannan/hadoop/conf nannan@amaranth2:/home/nannan/hadoop/conf
+sudo rsync -avxP /home/nannan/hadoop/conf nannan@amaranth3:/home/nannan/hadoop/conf
+sudo rsync -avxP /home/nannan/hadoop/conf nannan@amaranth4:/home/nannan/hadoop/conf
+sudo rsync -avxP /home/nannan/hadoop/conf nannan@amaranth5:/home/nannan/hadoop/conf
+sudo rsync -avxP /home/nannan/hadoop/conf nannan@amaranth6:/home/nannan/hadoop/conf
