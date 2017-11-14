f60f1920d184cd32ab593a3b1b84f4a5c4172f78
nnzhaocs <znannan1@vt.edu>
2017-11-14 15:20:46 -0500

    install spark

--- scripts/spark-code/install_spark.sh
+++ scripts/spark-code/install_spark.sh
@@ -1,2 +1,5 @@
 
+cd ~/Downloads
 wget http://mirrors.ocf.berkeley.edu/apache/spark/spark-2.2.0/spark-2.2.0-bin-hadoop2.7.tgz
+tar -zxvf spark-2.2.0-bin-hadoop2.7.tgz
+
---
+++ scripts/spark-code/mv_spark_dir.sh
@@ -0,0 +1,3 @@
+sudo mv /home/nannan/Downloads/spark-2.2.0-bin-hadoop2.7/ /usr/local/
+sudo gvfs-trash /usr/local/spark
+sudo ln -s /usr/local/spark-2.2.0-bin-hadoop2.7 /usr/local/spark
