502786eb5c267d1cd4925c64a4218e831ba34e05
nnzhaocs <znannan1@vt.edu>
2017-12-14 18:07:59 -0500

    deduplication analysis getting size

--- scripts/spark-code/deduplication_analysis.py
+++ scripts/spark-code/deduplication_analysis.py
@@ -6,7 +6,8 @@ from itertools import chain
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
-from pyspark.sql.functions import col
+from pyspark.sql.functions import col, collect_list, size
+from pyspark.sql import functions as F
 
 
 HDFS_DIR = 'hdfs://hulk0:8020/'
@@ -28,6 +29,10 @@ layer_db_absfilename3 = os.path.join(LAYER_DB_JSON_DIR, "nannan_2tb_json.parquet
 
 
 output_absfilename = os.path.join(VAR_DIR, 'manifests.parquet')
+output_unique_filename = os.path.join(LOCAL_DIR, 'unique_file_ids.parquet')
+output_unique_cnt_filename = os.path.join(LOCAL_DIR, 'unique_file_cnts.parquet') 
+output_uniq_files_cnts_bigger_1 = os.path.join(LOCAL_DIR, 'output_uniq_files_cnts_bigger_1.parquet')
+
 
 """.set("spark.executor.cores", 5) \
     .set("spark.driver.memory", "10g") \
@@ -47,13 +52,57 @@ all_json_absfilename = os.path.join(LOCAL_DIR, 'manifest.lst')
 
 
 def calculate_redundant_files_in_layers(spark, sc):
+    cnt_bigger_1 = spark.read.parquet(output_uniq_files_cnts_bigger_1)
+    sha256 = cnt_bigger_1.select("sha256")
+    
     layer_db_df = spark.read.parquet(LAYER_DB_JSON_DIR)
     files = layer_db_df.selectExpr("explode(dirs) As structdirs").selectExpr(
         "explode(structdirs.files) As structdirs_files").selectExpr("structdirs_files.*")
-    sha256 = files.select(files.sha256)
+    regular_files = files.filter(files.sha256.isNotNull())
 
-    sha256.show()
-    print(sha256.count())
+    cnt_bigger_1_df = regular_files.join(cnt_bigger_1, ['sha256'], 'leftsemi')
+    cnt_bigger_1_df.show()
+    #cnt_bigger_1_df.write.save()
+    
+
+def save_unique_files_cnt_bigger_1(spark, sc):
+    cnt = spark.read.parquet(output_unique_cnt_filename)
+    cnt_bigger_1 = cnt.where("count > 1")
+    cnt_bigger_1.show()
+    print(cnt_bigger_1.count())
+    cnt_bigger_1.write.save(output_uniq_files_cnts_bigger_1)
+
+
+def save_unqiue_files_cnts(spark, sc):
+    layer_db_df = spark.read.parquet(LAYER_DB_JSON_DIR)
+    files = layer_db_df.selectExpr("explode(dirs) As structdirs").selectExpr(
+        "explode(structdirs.files) As structdirs_files").selectExpr("structdirs_files.*")
+    regular_files = files.filter(files.sha256.isNotNull())
+    #print(regular_files.count())
+    #uniq_files_sha256 = spark.read.parquet(output_unique_filename)
+    #uniq_rows = regular_files.filter(regular_files.sha256.isin(uniq_files_sha256.sha256))
+    
+    #uniq_rows.groupby(uniq_rows.sha256).agg(F.collect_list(uniq_rows.file_info.stat_size)
+    cnt = regular_files.groupby(regular_files.sha256).count().distinct()#agg(F.collect_list(regular_files.file_info.stat_size))
+    cnt.write.save(output_unique_cnt_filename)
+
+
+def save_unique_files(spark, sc):
+    layer_db_df = spark.read.parquet(LAYER_DB_JSON_DIR)
+    files = layer_db_df.selectExpr("explode(dirs) As structdirs").selectExpr(
+        "explode(structdirs.files) As structdirs_files").selectExpr("structdirs_files.*")
+    regular_files = files.filter(files.sha256.isNotNull())
+    uniq_files = regular_files.select(regular_files.sha256).dropDuplicates()
+    """file_group = regular_files \
+		.agg(collect_list(regular_files.sha256) \
+		.alias("ids")) \
+		.where(size("ids") > 1) """
+    uniq_files.show()
+    uniq_files.write.save(output_unique_filename)
+    #sha256.show()
+    #unique_sha = sha256.dropDuplicates()
+    
+    #print(sha256.count())
 
 
 def init_spark_cluster():
