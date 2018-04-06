8ac3c7033004fb6d36c32234ba08b72abe6de8d9
nnzhaocs <znannan1@vt.edu>
2018-04-06 14:37:41 -0400

    compression test

--- scripts/analyzer/test_decompression_methods.py
+++ scripts/analyzer/test_decompression_methods.py
@@ -1,13 +1,10 @@
 
 from config import *
-
-"""
-
-"""
+from decompression_methods import *
 
 def test_decompression():
     layer_dict = load_layer_dict()
-    layer_fnames = load_layer_dict(layer_dict)
+    layer_fnames = load_testing_layer_lst(layer_dict)
 
     print "create pool"
     P = multiprocessing.Pool(num_worker_process)
@@ -31,7 +28,11 @@ def load_testing_layer_lst(layer_dict):
     with open(testing_layer_lst_absfilename) as f:
         for line in f:
             line = line.rstrip('\n')
-            layer_filename = layer_dict[line]
+            try:
+                layer_filename = layer_dict[line]
+            except:
+                print("Cannot find layer_id in this machine's layer directories ################ ", line)
+                continue
             layer_fnames.append(layer_filename)
 
     return layer_fnames
@@ -44,7 +45,7 @@ def load_layer_dict():
     return layer_dict
 
 
-def process_layer(layer_filename, compression_methods):
+def process_layer(layer_filename):
     processname = 0 #multiprocessing.current_process().name
     logging.debug("[%s] process layer_filename: %s", processname, layer_filename)
 
@@ -78,23 +79,25 @@ def process_layer(layer_filename, compression_methods):
         return False
 
     start = time.time()
+
     filetype = check_file_type(layer_filename)
+
     if filetype == 'tar':
         print "This is a tar file"
         archival_size = os.lstat(os.path.join(dest_dir[0]['layer_dir'], layer_filename)).st_size
         logging.debug("archival_size %d B, name: %s", archival_size, layer_filename)
-        sub_dirs, compressed_size_with_method_gzip = load_dirs(layer_filename, filetype)
+        compressed_archival_size, -1 = load_dirs(layer_filename, filetype)
         elapsed = time.time() - start
         logging.info('process layer_id:%s : total time, consumed time ==> %f s; compress size: %d',
-                     layer_filename, elapsed, compressed_size_with_method_gzip)
+                     layer_filename, elapsed, compressed_archival_size)
     elif filetype == 'gzip':
         print "This is a gzip file"
-        compressed_size_with_method_gzip = os.lstat(os.path.join(dest_dir[0]['layer_dir'], layer_filename)).st_size
+        # compressed_archival_size = os.lstat(os.path.join(dest_dir[0]['layer_dir'], layer_filename)).st_size
         logging.debug("compressed_size_with_method_gzip %d B, name: %s", compressed_size_with_method_gzip, layer_filename)
-        sub_dirs, archival_size = load_dirs(layer_filename, filetype)
+        compressed_archival_size, archival_size = load_dirs(layer_filename, filetype)
         elapsed = time.time() - start
         logging.info('process layer_id:%s : total time, consumed time ==> %f s; compress size: %d',
-                     layer_filename, elapsed, compressed_size_with_method_gzip)
+                     layer_filename, elapsed, compressed_archival_size)
     else:
         logging.info('################### The layer tarball type is neither tar or gzip! layer file name %s ###################', layer_filename)
         return
@@ -106,7 +109,7 @@ def process_layer(layer_filename, compression_methods):
         logging.debug('################### The layer tarball has problems! layer file name %s ###################', layer_filename)
         return
 
-    if archival_size == -1 or compressed_size_with_method_gzip == -1:
+    if archival_size == -1 or compressed_archival_size == -1:
         return
 
     # depths = [sub_dir['dir_depth'] for sub_dir in sub_dirs if sub_dir]
@@ -117,21 +120,20 @@ def process_layer(layer_filename, compression_methods):
     #     'dir_avg_depth': statistics.mean(depths)
     # }
 
-    sha, id, timestamp = str(layer_filename).split("-")
-
+    # sha, id, timestamp = str(layer_filename).split("-")
     size = {
-        'uncompressed_sum_of_files': sum_layer_size(sub_dirs),
-        'compressed_size_with_method_gzip': compressed_size_with_method_gzip,
+        # 'uncompressed_sum_of_files': sum_layer_size(sub_dirs),
+        'compressed_size_with_method'+'_'+method: compressed_archival_size,
         'archival_size': archival_size   # archival_size
     }
 
     layer = {
         'layer_id': sha+':'+id,  # str(layer_id).replace("/", ""),
-        'dirs': sub_dirs,  # getLayersBychainID(chain_id),
+        # 'dirs': sub_dirs,  # getLayersBychainID(chain_id),
         # 'layer_depth': dir_depth,
         'size': size,  # sum of files size,
         # 'repeats': 0,
-        'file_cnt': sum_file_cnt(sub_dirs)
+        # 'file_cnt': sum_file_cnt(sub_dirs)
     }
 
     abslayer_filename = os.path.join(layer_db_json_dir, layer_filename+'.json')
