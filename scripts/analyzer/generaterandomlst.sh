5884a7c76ce3fca7b75de77c6f728daf38c7547b
nnzhaocs <znannan@vt.edu>
2019-12-28 15:32:46 -0500

    update

--- scripts/analyzer/generaterandomlst.sh
+++ scripts/analyzer/generaterandomlst.sh
@@ -6,5 +6,5 @@ for (( i = 350 ; i < 400 ; i += 1 )) ; do
 
         echo $i
         #./server.sh $i
-	python random_select_and_packing.py $inputfile $i $outputfile_$i
+	python random_select_and_packing.py $inputfile $i "$outputfile_$i"
 done
