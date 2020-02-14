fe9a65ad8a782c1beeb4f3f57bf651b5e4014138
nnzhaocs <znannan1@vt.edu>
2020-02-13 19:04:32 -0500

    update

--- benchmarkdockercontainer/iobenchmark/cow_test_layers.sh
+++ benchmarkdockercontainer/iobenchmark/cow_test_layers.sh
@@ -1,6 +1,6 @@
 
 echo "copy on write test"
-#================ inital parameters================
+#================ distributions ================
 
 nrfss=(90 2600 50000)
 
@@ -14,24 +14,46 @@ layersizes=("3k" "14k" "8000k" "55000k" "900000k")
 
 imgsizes=("190m" "280m" "530m" "800m" "5000m")
 
-#layers=(6 10 12 19 50)
+layers=(6 10 12 19 50)
+
+#================== init parameters =============================
+
+nrfs=90
+filesize="1m"
+
+rwtp="randwrite"
+blksize=4
+blocksize="${blksize}k"
 
 nrjobs=1
 totalruntime=600
 
-nrfs=90
-filesize="1m"
-rwtp="randwrite"
-blocksize="1k"
+testmode="container" # or rawfs or container
 
 #testsize="2g"
 #testsize=$(echo "$nrfs * $filesize"|bc)
-# testsize=45m
-# 
-#=============================================
-echo "creating a testing layer"
+#testsize=45m
+ 
+#=================== overwrite parameters ========================
 
-create_img () {
+tsize=$(echo "$blksize * $nrfs * 3" | bc)
+testsize="${tsize}k" # 4k * 90 * 3 # testsize = (blocksize * nrfiles) * 3
+
+#==================== cleanup caches & disk cache ======================================
+
+echo "cleanup caches & disk cache"
+
+echo 1 > /proc/sys/vm/drop_caches
+hdparm -W 0 /dev/sda
+
+
+#================== create img and files =======================
+
+echo "create img and files"
+
+create_imgwithfiles () {
+
+	echo "create_imgwithfiles"
 
 	docker run --name test_cow --rm nnzhaocs/fio sleep 910 &
 	sleep 5
@@ -42,80 +64,64 @@ create_img () {
 	docker stop $(docker ps -a -q)
 }
 
-init_ext4 () {
-	fio --name=cowtest --nrfiles="$nrfs" --filesize="${filesize}" --filename_format='/$jobnum.$filenum.f' --bs=$blocksize --direct=1 --rw=$rwtp --allow_file_create=1 --numjobs=$nrjobs --runtime=$totalruntime --group_reporting
+create_files () {
+
+	echo "create files"
+
+        fio --name=cowtest --nrfiles="$nrfs" --filesize="${filesize}" --filename_format='/$jobnum.$filenum.f' --bs=$blocksize --direct=1 --rw=$rwtp --allow_file_create=1 --numjobs=$nrjobs --runtime=$totalruntime --group_reporting
 }
 
-#==============================================
-# overwrite parameters:
 
-testsize="6m" # testsize < (filesize * nrfiles) / 2
-
-
-#==============================================
-echo "test overwrite!"
-
-test_ext4 () {
-	sync; echo 1 > /proc/sys/vm/drop_caches
-
-        #testsize="45m"
-        #nrfs=45
-
+rewrite_files () {
+	echo "rewrite files"
 	fio --name=cowtest --nrfiles="$nrfs" --filename_format='/$jobnum.$filenum.f' --bs=$blocksize --direct=1 --rw=$rwtp --numjobs=$nrjobs --runtime=$totalruntime --group_reporting --ioengine=sync --fdatasync=1 --size=$testsize --file_service_type=random
 
 }
 
-#totallayercnt=2
 
-add_layers () {
+rewrite_layer () {
 
-	i=0
+	echo "rewrite to previous layer"
 
-	while [ $i -lt 50  ] ; do
+	docker run --name test-0 --rm -m 0 nnzhaocs/fio_cow-0 sleep 250 &
 
-    		echo "previous layer cnt: $i"
+	sleep 5
 
-    		sync; echo 1 > /proc/sys/vm/drop_caches
+	docker exec -ti test-0 fio --name=cowtest --nrfiles="$nrfs" --filename_format='/$jobnum.$filenum.f' --bs=$blocksize --direct=1 --rw=$rwtp --numjobs=$nrjobs --runtime=$totalruntime --group_reporting --ioengine=sync --fdatasync=1 --size=$testsize --file_service_type=random 
 
-    		#testsize="50m"
-		#nrfs=100
+	echo "rewriting and creating a layer"
+	sleep 2
+	docker commit test-0 nnzhaocs/fio_cow-1
+	sleep 10
 
-    		docker run --name test-$i --rm -m 0 nnzhaocs/fio_cow-$i sleep 250 &
-
-    		sleep 5
-
-    		docker exec -ti test-$i fio --name=cowtest --nrfiles="$nrfs" --filename_format='/$jobnum.$filenum.f' --bs=$blocksize --direct=1 --rw=$rwtp --numjobs=$nrjobs --runtime=$totalruntime --group_reporting --ioengine=sync --fdatasync=1 --size=$testsize --file_service_type=random 
-
-    		layercnt=$(echo "$i + 1" | bc)
-    		#layercnt=$i
-    		echo "creating layer cnt: $layercnt"
-    		sleep 2
-    		docker commit test-$i nnzhaocs/fio_cow-$layercnt
-    		sleep 10
-		# sleep 150
-    		((i++))
-
-		docker stop $(docker ps -a -q)
-		# docker rm $(docker ps -a -q)
-		break
-	done
+	docker stop $(docker ps -a -q)
+	# docker rm $(docker ps -a -q)
 }
 
 # docker rmi -f $(docker ps -a -q)
 
 cowtest_container () {
 
-        create_img
-        add_layers
+        create_imgwithfiles
+	sleep 10
+        rewrite_layer
 }
 
-cowtest_ext4 () {
+cowtest_rawfs () {
 
-        init_ext4
-        test_ext4
+        create_files
+	sleep 10
+        rewrite_files
 }
 
-cowtest_ext4 
 
-#cowtest_container
+if [ "$testmode" == "container"] ; then
+	echo $testmode
+	cowtest_container
+else
+	echo "$testmode"
+	cowtest_rawfs
+fi
+
+
 
