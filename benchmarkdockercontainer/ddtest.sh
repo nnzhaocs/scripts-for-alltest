daaa93f691d8a2a4cbf203cf673abbf77e1b96f1
nnzhaocs <znannan1@vt.edu>
2020-02-13 22:01:32 -0500

    update

--- benchmarkdockercontainer/ddtest.sh
+++ benchmarkdockercontainer/ddtest.sh
@@ -1,8 +1,9 @@
 
 i=0
 
-while [ $i -lt 10  ] ; do
+while [ $i -lt 5  ] ; do
 	echo "$i: write 5g = 64k*81920"
-	dd if=/dev/zero of=/home/testfile bs=64k count=81920 oflag=direct
+	dd if=/dev/zero of=/home/testfile bs=1G count=2 oflag=direct
+	sleep 10
 	rm -rf /home/testfile
 done
