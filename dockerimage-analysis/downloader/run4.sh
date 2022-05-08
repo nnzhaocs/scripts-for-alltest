4bee85b60e336d3cf843db306a926bd7b989ea3d
nnzhaocs <znannan1@vt.edu>
2022-05-08 15:35:47 +0800

    update run4.sh

--- dockerimage-analysis/downloader/run4.sh
+++ dockerimage-analysis/downloader/run4.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+============run downloader as a docker image!!!==========================
+
+mkdir ~/Downloads; touch ~/Downloads/downloadedlayers.lst; touch ~/Downloads/downloadedimages.lst
+
+mv xxxx.lst ~/Downloads/image_name.lst
+
+docker build ./ -t downloader
+
+docker run -v ~/Downloads/:/root/Downloads downloader
+
+==========================================================================
 date >> timestamp.out
 python auto_download_compressed_images.py -f images-names/xdj -d /gpfs/docker_images_largefs/xdj/
 date >> timestamp.out
